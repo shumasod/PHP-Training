c2e63d4dcb0b92dfddca8a090112681226243a6f
ShuBox <64628603+shumasod@users.noreply.github.com>
2023-12-07 22:10:03 +0900

    Update validation.php

--- validation.php
+++ validation.php
@@ -1,11 +1,11 @@
 <?php
-
-function validation($request){//$_POST連想配列
+//$_POST連想配列
+function validation($request){
 
 $errors = [];
 
-if(empty($request['your_name'])){
-    $errors[] = '氏名は必須です';
+if(empty($request['your_name'])|| 20<mb<strlen($request['your_name']))){
+    $errors[] = '氏名は必須です.20文字以内で入力してください。';
 }
 
 return $errors;
