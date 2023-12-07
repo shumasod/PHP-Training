eaddc28ed89fa7e3a584ea7cefc40fefe39b65a6
ShuBox <64628603+shumasod@users.noreply.github.com>
2023-12-07 23:10:15 +0900

    Update validation.php

--- validation.php
+++ validation.php
@@ -8,6 +8,10 @@ if(empty($request['your_name'])|| 20<mb<strlen($request['your_name']))){
     $errors[] = '氏名は必須です.20文字以内で入力してください。';
 }
 
+    if(empty($request['contact'])|| 20<mb<strlen($request['contact']))){
+    $errors[] = 'お問い合わせ内容は必須です.200文字以内で入力してください。';
+}
+
 return $errors;
 
 }
