d625acb99cd02a2b49228b134cbb7e2811050419
ShuBox <64628603+shumasod@users.noreply.github.com>
2023-12-08 19:02:48 +0900

    Update validation.php

--- validation.php
+++ validation.php
@@ -1,28 +1,41 @@
 <?php
 //$_POST連想配列
 function validation($request){
-
+}
 $errors = [];
 
-if(empty($request['your_name'])|| 20<mb<strlen($request['your_name']))){
+if(empty($request['your_name'])|| 20<mb_strlen($request['your_name'])){
     $errors[] = '氏名は必須です.20文字以内で入力してください。';
 }
-if(!iseset($request['gender'])){
-    if(!isset($errors[] = `「性別」は必須です.';
+
+if(empty($request['email']) || !filter_var($request['email'], FILTER_VALIDATE_EMAIL)){
+    $errors[] = '「メールアドレス」は必須です。正しい形式で入力してください。';
+}
+
+if(!empty($request['url'])){
+    if(!filter_var($request['url'] , FILTER_VALIDATE_URL)){
+        $errors[] = '「ホームページ」は正しい形式で入力してください';
     }
-if(empty($request['age'])){
-   $error[]= '「年齢」は必須です。';
+}
+
+if(!isset($request['gender'])){
+    $errors[] = '「性別」は必須です.';
+    }
+
+if(empty($request ['age']|| 6 < $request ['age'])){
+    $errors []= '「年齢」は必須です。';
     
-    if(empty($request['contact'])|| 20<mb<strlen($request['contact']))){
-    $errors[] = 'お問い合わせ内容は必須です.200文字以内で入力してください。';
+    if(empty($request ['contact'])|| 20< mb_strlen($request ['contact'])){
+    $errors [] = 'お問い合わせ内容は必須です.200文字以内で入力してください。';
 }
 
 return $errors;
 
 }
 
-if(empty($request['caution'])){
-    $errors[]= '「注意事項をご確認ください。';
+if(empty($request ['caution'])) {
+    $errors [] =  '注意事項をご確認ください。';
 }
+
 
 ?>
