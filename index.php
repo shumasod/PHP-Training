fc84bbe58518d2b02e27a6a9843e5a15344759e8
ShuBox <64628603+shumasod@users.noreply.github.com>
2023-12-08 19:03:50 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,16 +1,68 @@
 <?php
-$title='お勉強サイト';
 
-ob_start();
-require 'header.php';
-$contests = ob_get_clean();
-echo $contests;
+$array = [1, 2, 3];
 
-//Contestsの表示
+$array_2 = [
+    ['赤', '青', '黄'],
+    ['緑', '紫', '黒'] // 修正: '紫' と '黒' を別々の要素として追加
+];
 
-require 'rss.php';
-require 'main.php';
-require 'sub1.php';
-require 'sub2.php';
-require 'footer.php';
+echo '<pre>';
+var_dump($array_2);
+echo '</pre>';
+
+$array_member = [
+    'name' => '本田',
+    'height' => 170,
+    'hobby' => 'サッカー'
+];
+
+echo $array_member['hobby'];
+
+echo '<pre>';
+var_dump($array_member);
+echo '</pre>';
+
+$array_member_2 = [
+    '本田' => [
+        'height' => 170,
+        'hobby' => 'サッカー'
+    ],
+    '香川' => [
+        'height' => 165,
+        'hobby' => 'サッカー'
+    ]
+];
+
+echo $array_member_2['香川']['height'];
+
+echo '<pre>';
+var_dump($array_member_2);
+echo '</pre>';
+
+$array_member_3 = [
+    '1kumi' => [
+        // 修正: 1組の要素を追加
+        '佐藤' => [
+            'height' => 175,
+            'hobby' => 'バスケットボール'
+        ]
+    ],
+    '2kumi' => [
+        '長友' => [
+            'height' => 160,
+            'hobby' => 'サッカー'
+        ],
+        '乾' => [
+            'height' => 168,
+            'hobby' => 'サッカー'
+        ]
+    ]
+];
+
+echo $array_member_3['2kumi']['長友']['height'];
+
+echo '<pre>';
+var_dump($array_member_3);
+echo '</pre>';
 ?>
