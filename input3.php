7d7e61672a5f500d36231f37aa89ad7a4d3d9790
ShuBox <64628603+shumasod@users.noreply.github.com>
2023-12-11 23:44:39 +0900

    Update input3.php

--- input3.php
+++ input3.php
@@ -2,6 +2,8 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $email = $_POST["email"];
+    $gender = isset($_POST["gender"]) ? $_POST["gender"] : '';
+    $homepage = $_POST["homepage"];
     $message = $_POST["message"];
 
     // ここでデータベースに保存したり、メール送信したり、適切な処理を行います。
@@ -10,6 +12,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "<h2>お問い合わせ内容</h2>";
     echo "<p><strong>お名前:</strong> $name</p>";
     echo "<p><strong>メールアドレス:</strong> $email</p>";
+    echo "<p><strong>性別:</strong> $gender</p>";
+    echo "<p><strong>ホームページURL:</strong> $homepage</p>";
     echo "<p><strong>お問い合わせ内容:</strong><br> $message</p>";
 } else {
     // POST メソッド以外でアクセスされた場合のエラーハンドリング
@@ -25,11 +29,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <title>お問い合わせフォーム</title>
     <!-- Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        body {
+            background-color: #f8f9fa;
+        }
+
+        .container {
+            background-color: #fff;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            padding: 20px;
+            margin-top: 50px;
+        }
+    </style>
 </head>
 <body>
 
-<div class="container mt-5">
-    <h2>お問い合わせフォーム</h2>
+<div class="container">
+    <h2 class="mb-4">お問い合わせフォーム</h2>
     <form action="process_form.php" method="post">
         <div class="form-group">
             <label for="name">お名前:</label>
@@ -38,6 +55,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <div class="form-group">
             <label for="email">メールアドレス:</label>
             <input type="email" class="form-control" id="email" name="email" required>
+        </div>
+        <div class="form-group">
+            <label>性別:</label>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="gender" id="gender_male" value="男性">
+                <label class="form-check-label" for="gender_male">男性</label>
+            </div>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="gender" id="gender_female" value="女性">
+                <label class="form-check-label" for="gender_female">女性</label>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="homepage">ホームページURL:</label>
+            <input type="url" class="form-control" id="homepage" name="homepage">
         </div>
         <div class="form-group">
             <label for="message">お問い合わせ内容:</label>
