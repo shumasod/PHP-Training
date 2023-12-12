31d644f0e731a11f4d5364c3e9d4e2cb77b99eb0
ShuBox <64628603+shumasod@users.noreply.github.com>
2023-12-12 19:04:32 +0900

    Add files via upload

---
+++ extends.php
@@ -0,0 +1,60 @@
+<?php
+
+// 親クラス
+class BaseProduct {
+    // 変数 関数
+    public function echoProduct() {
+        echo '親クラスです';
+    }
+
+    // オーバーライド（上書き）
+    public function getProduct() {
+        echo '親の関数です';
+    }
+}
+
+// 子クラス
+class Product extends BaseProduct {
+
+    // アクセス修飾子、private, protected(自分・継承したクラス), public
+
+    // 変数
+    private $product = '';
+
+    // 関数
+    function __construct($product) {
+        $this->product = $product;
+    }
+
+    public function getProduct() {
+        echo $this->product;
+    }
+
+    public function addProduct($item) {
+        $this->product .= $item;
+    }
+
+    public static function getStaticProduct($str) {
+        echo $str;
+    }
+}
+
+$instance = new Product('テスト');
+
+var_dump($instance);
+
+$instance->getProduct();
+echo '<br>';
+
+// 親クラスのメソッド
+$instance->echoProduct();
+echo '<br>';
+
+$instance->addProduct('追加分');
+echo '<br>';
+
+$instance->getProduct();
+echo '<br>';
+
+Product::getStaticProduct('静的');
+echo '<br>';
