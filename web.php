7275a65dfc4527ac60e7fe062d7dc189ba2b28f9
ShuBox <64628603+shumasod@users.noreply.github.com>
2023-12-25 19:57:40 +0900

    Update web.php

--- web.php
+++ web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\ContactFormController;
+use App\Http\Controllers\ShopController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +16,35 @@ use App\Http\Controllers\TestController;
 |
 */
 
+Route::get('tests/test', [ TestController::class, 'index' ]);
+
+Route::get('shops', [ ShopController::class, 'index' ]);
+
+// Route::resource('contacts', ContactFormController::class);
+
+
+
+Route::prefix('contacts')
+->middleware(['auth'])
+->controller(ContactFormController::class)
+->name('contacts.')
+->group(function(){
+    Route::get('/', 'index')->name('index');
+    Route::get('/create', 'create')->name('create');
+    Route::post('/', 'store')->name('store');
+    Route::get('/{id}', 'show')->name('show');
+    Route::get('/{id}/edit', 'edit')->name('edit');
+    Route::post('/{id}', 'update')->name('update');
+    Route::post('/{id}/destroy', 'destroy')->name('destroy');
+});
+
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('tests/test',[TestController::class, 'index' ] );
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
