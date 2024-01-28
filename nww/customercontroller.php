50b98bb5dd7f60edf0ce6e68b60d57f5aac0846f
ShuBox <64628603+shumasod@users.noreply.github.com>
2024-01-28 18:23:05 +0900

    Update customercontroller.php

--- nww/customercontroller.php
+++ nww/customercontroller.php
@@ -26,3 +26,45 @@ class CustomerController extends Controller
         return view('dashboard', compact('customers'));
     }
 }
+
+
+##TestControllers
+
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Test; // この行を追加
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
+
+class TestController extends Controller
+{
+    public function test();{
+        $users = user::all();
+        return view('test',compact('users'));
+        }
+    
+
+    
+    
+        // Eloquent(エロクアント)
+        $values = Test::all();
+
+        $count = Test::count();
+
+        $first = Test::findOrFail(1);
+
+        $whereBBB = Test::where('text', '=', 'bbb')->get();
+
+        // クエリビルダ
+        $queryBuilder = DB::table('tests')->where('text', '=', 'bbb')
+        ->select('id', 'text')
+        ->first();
+
+        dd($values, $count, $first, $whereBBB, $queryBuilder);
+
+        return view('tests.test', compact('values'));
+    }
+}
