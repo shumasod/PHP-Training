6d13dc68c57bf94ef324b738560a8efce2303c4a
ShuBox <64628603+shumasod@users.noreply.github.com>
2024-01-28 18:19:52 +0900

    Create customercontroller.php

---
+++ nww/customercontroller.php
@@ -0,0 +1,28 @@
+
+
+// app/Http/Controllers/CustomerController.php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Customer;
+
+class CustomerController extends Controller
+{
+    public function index()
+    {
+        $customers = Customer::all();
+        return view('dashboard', compact('customers'));
+    }
+
+    public function search(Request $request)
+    {
+        $search = $request->input('search');
+
+        $customers = Customer::where('name', 'like', '%' . $search . '%')
+            ->orWhere('email', 'like', '%' . $search . '%')
+            ->get();
+
+        return view('dashboard', compact('customers'));
+    }
+}
