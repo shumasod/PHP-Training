fd553dcb613a928607c8c17ea3440331ccbc4fa6
ShuBox <64628603+shumasod@users.noreply.github.com>
2024-01-28 18:16:19 +0900

    Create web.php

---
+++ nww/web.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
+use App\Http\Controllers\ContactFormController;
+use App\Http\Controllers\CustomerController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+    // Route:: get('tests/test', [ TestController::class, 'index' ]);
+
+
+
+// Route::resource('contacts', ContactFormController::class);
+
+
+
+Route::prefix('contacts')
+->middleware(['auth'])
+->controller(ContactFormController::class)
+->name('contacts.')
+->group(function(){
+    Route::get('/', 'index')->name('index');
+    Route::get('/create', 'create')->name('create');
+    Route::post('/', 'store')->name('store');
+    Route::get('/{id}', 'show')->name('show');
+    Route::get('/{id}/edit', 'edit')->name('edit');
+    Route::post('/{id}', 'update')->name('update');
+    Route::post('/{id}/destroy', 'destroy')->name('destroy');
+});
+
+routes/web.php
+
+
+
+Route::get('/dashboard', [CustomerController::class, 'index'])->name('dashboard');
+Route::get('/dashboard/search', [CustomerController::class, 'search'])->name('dashboard.search');
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+}) ;
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
