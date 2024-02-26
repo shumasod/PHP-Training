9997b858c3e1dfb1388847467018c2d4888bb2aa
shubox <shumasod@gmail.com>
2024-02-26 21:58:40 +0900

    練習

--- nww/customercontroller.php
+++ nww/customercontroller.php
@@ -14,7 +14,7 @@ class CustomerController extends Controller
 
     public function search(Request $request)
     {
-        $search = $request->input('search');
+        $search = $request->input('search')
 
         $customers = Customer::where('name', 'like', '%' . $search . '%')
             ->orWhere('email', 'like', '%' . $search . '%')
@@ -39,7 +39,7 @@ use App\Models\User;
 class TestController extends Controller
 {
 <<<<<<< HEAD
-    pubilc  function test(); {
+public function test() {
 =======
     public function test();{ 
 >>>>>>> 2d7621099f058b089451b2e28d314f3b5c2daab8
