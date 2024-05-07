994d71ca4a56260ff71fe46d6b3b21317a0d97db
ShuBox <64628603+shumasod@users.noreply.github.com>
2024-05-07 19:26:19 +0900

    Update customercontroller.php

--- nww/customercontroller.php
+++ nww/customercontroller.php
@@ -1,4 +1,3 @@
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -14,7 +13,7 @@ class CustomerController extends Controller
 
     public function search(Request $request)
     {
-        $search = $request->input('search')
+        $search = $request->input('search'); // 文法エラーの修正（セミコロン追加）
 
         $customers = Customer::where('name', 'like', '%' . $search . '%')
             ->orWhere('email', 'like', '%' . $search . '%')
@@ -23,52 +22,3 @@ class CustomerController extends Controller
         return view('dashboard', compact('customers'));
     }
 }
-
-
-##TestControllers
-
-<?php
-
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-
-use Illuminate\Support\Facades\DB;
-use App\Models\User;
-
-class TestController extends Controller
-{
-<<<<<<< HEAD
-public function test() {
-=======
-    public function test();{ 
->>>>>>> 2d7621099f058b089451b2e28d314f3b5c2daab8
-        $users = user::all();
-        return view('test',compact('users'));
-        }
-    
-
-    
-        <?php
-
-        namespace App\Http\Controllers;
-        
-        use Illuminate\Http\Request;
-        use Illuminate\Support\Facades\DB;
-        use App\Models\User;
-        use App\Models\Test; // 追加
-        
-        class TestController extends Controller
-        {
-            public function test() {
-                // クエリビルダ
-                $queryBuilder = DB::table('tests')->where('text', '=', 'bbb')
-                ->select('id', 'text')
-                ->first();
-        
-                dd($queryBuilder);
-        
-                return view('tests.test');
-            }
-        }
-        
