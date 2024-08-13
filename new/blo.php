b80c538568f1799da896d6e1a962103d28dc0ea0
ShuBox <64628603+shumasod@users.noreply.github.com>
2024-08-13 22:33:15 +0900

    blo.php を更新

--- new/blo.php
+++ new/blo.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>あなたのブログ</title>
+  <title>ブログ</title>
   <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
   <link rel="icon" href="<?php echo asset('images/macho-icon.ico'); ?>" type="image/x-icon">
 </head>
