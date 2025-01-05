41242674f16638279a2f631eb26ec75abb522e36
ShuBox <64628603+shumasod@users.noreply.github.com>
2025-01-05 18:42:00 +0900

    Create Notification.php

---
+++ Mailble/Notification.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class NotificationMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $mailData;
+
+    public function __construct($mailData)
+    {
+        $this->mailData = $mailData;
+    }
+
+    public function build()
+    {
+        return $this->subject('メールタイトル')
+                    ->view('emails.notification')
+                    ->with([
+                        'mailData' => $this->mailData
+                    ]);
+    }
+}
